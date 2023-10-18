0c564ec6b33622cf6248b7ebad97011b1657b31d
Kornelius <magnus.korneliusson@gmail.com>
2023-10-18 22:37:55 +0200

    Initial commit.

---
+++ Day01.cs
@@ -0,0 +1,16 @@
+using AoCHelper;
+using System.Threading.Tasks;
+
+namespace AOC_2017;
+class Day01 : BaseDay
+{
+    public override ValueTask<string> Solve_1()
+    {
+        return new("Solution 1");
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using AoCHelper;
+
+namespace AOC_2017;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Solver.SolveLast();
+    }
+}
